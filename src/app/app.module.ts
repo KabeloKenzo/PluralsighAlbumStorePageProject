a3d4af1c95590dfe5e66a5bd8438bfa2cfd9ecb7
KabeloKenzo <oneelementdesign@gmail.com>
2021-12-13 15:36:53 +0200

    Completed module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductDescriptionComponent } from './product-description/product-description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductDescriptionComponent
   ],
   imports: [
     BrowserModule,
